005217b6eec8f92891ec9cd482cbe425b9564053
Tzvi Neiman <tneiman19@gmail.com>
2023-01-25 23:59:37 -0500

    1) Initial commit 2) added code to save the user input from the selected text aread to local storage 3) added date to top of the screen using the moment api

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(function () {
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+  //
+  $(".saveBtn").click(function () {
+    var parent = $(this).parent();
+    var parentId = parent.attr("id");
+    console.log(parentId);
+    var enteredText = parent.find("textarea").val();
+    console.log(enteredText);
+    localStorage.setItem(parentId, enteredText);
+  });
+
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  //
+
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
+
+  var testArray = [
+    "hour-9",
+    "hour-10",
+    "hour-11",
+    "hour-12",
+    "hour-1",
+    "hour-2",
+    "hour-3",
+    "hour-4",
+    "hour-5",
+  ];
+
+  for (var i = 0; i < testArray.length; i++) {
+    var lsItem = localStorage.getItem(testArray[i]);
+    console.log(lsItem);
+  }
+
+  // TODO: Add code to display the current date in the header of the page.
+  $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+});
